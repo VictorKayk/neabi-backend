9d68d2a0107cbe0bb9b9d7d16b99a924f4dcf079
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 19:56:04 -0300

    test: ensure Email return an error if email is too big

--- tests/domain/entities/email.spec.ts
+++ tests/domain/entities/email.spec.ts
@@ -13,4 +13,25 @@ describe('Email', () => {
     expect(error.isError()).toBe(true);
     expect(error.value).toEqual(new InvalidEmailError(''));
   });
+
+  it('Should return an error if email is larger than 320 chars', () => {
+    const email = `${'invalid'.repeat(10)}@${'invalid'.repeat(19)}.${'invalid'.repeat(19)}`;
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if domain part is larger than 255 chars', () => {
+    const email = `invalid@${'invalid'.repeat(19)}.${'invalid'.repeat(19)}`;
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if local part is larger than 64 chars', () => {
+    const email = `${'invalid'.repeat(10)}@invalid.com`;
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
 });

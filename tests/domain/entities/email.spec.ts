cb22fa3ac8574dbb694daa8d8351019476e8e945
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 19:36:55 -0300

    test: ensure Email return an error if email is empty

--- tests/domain/entities/email.spec.ts
+++ tests/domain/entities/email.spec.ts
@@ -1,9 +1,16 @@
 import { Email } from '@/entities/email';
+import { InvalidEmailError } from '@/entities/errors/invalid-email-error';
 
 describe('Email', () => {
   it('Should create if email is valid', () => {
     const email = Email.create('valid_email@test.com');
     expect(email.isSuccess()).toBe(true);
     expect(email.value).toEqual({ value: 'valid_email@test.com' });
+  });
+
+  it('Should return an error if email is empty', () => {
+    const error = Email.create('');
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(''));
   });
 });

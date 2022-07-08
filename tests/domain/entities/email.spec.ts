ae96fc891e49fd2c99937e0b58ca391cbd95bc2b
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 22:14:41 -0300

    test: ensure email validation

--- tests/domain/entities/email.spec.ts
+++ tests/domain/entities/email.spec.ts
@@ -3,9 +3,9 @@ import { InvalidEmailError } from '@/entities/errors/invalid-email-error';
 
 describe('Email', () => {
   it('Should create if email is valid', () => {
-    const email = Email.create('valid_email@test.com');
+    const email = Email.create('valid_email@it.com');
     expect(email.isSuccess()).toBe(true);
-    expect(email.value).toEqual({ value: 'valid_email@test.com' });
+    expect(email.value).toEqual({ value: 'valid_email@it.com' });
   });
 
   it('Should return an error if email is empty', () => {
@@ -30,6 +30,55 @@ describe('Email', () => {
 
   it('Should return an error if local part is larger than 64 chars', () => {
     const email = `${'invalid'.repeat(10)}@invalid.com`;
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if domain a part is larger than 63 chars', () => {
+    const email = `any@${'d'.repeat(64)}.com`;
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if local part is empty', () => {
+    const email = '@mail.com';
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if domain is empty', () => {
+    const email = 'any@';
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if local part have invalid char', () => {
+    const email = 'any email@mail.com';
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if local part have two dots', () => {
+    const email = 'any..email@mail.com';
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if local part have ending dot', () => {
+    const email = 'any.@mail.com';
+    const error = Email.create(email);
+    expect(error.isError()).toBe(true);
+    expect(error.value).toEqual(new InvalidEmailError(email));
+  });
+
+  it('Should return an error if email do not have an at-sign', () => {
+    const email = 'anymail.com';
     const error = Email.create(email);
     expect(error.isError()).toBe(true);
     expect(error.value).toEqual(new InvalidEmailError(email));

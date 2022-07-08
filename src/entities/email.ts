9b291bbd18da983cc6bd9466d98566be96a2f85f
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 22:14:33 -0300

    feat: ensure email validation

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,14 @@
 import { Either, success, error } from '@/shared';
 import { InvalidEmailError } from '@/entities/errors/invalid-email-error';
-import { isValid } from '@/entities/email-validation';
+
+function nonConformant(email: string): boolean {
+  const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+  return !emailRegex.test(email);
+}
+
+const isEmpty = (str: string): boolean => !str;
+const isTooLarge = (str: string, max: number): boolean => str.length > max;
 
 export class Email {
   readonly value: string;
@@ -10,8 +18,32 @@ export class Email {
     Object.freeze(this);
   }
 
+  public static isValid(email: string): boolean {
+    if (isEmpty(email)) return false;
+    if (nonConformant(email)) return false;
+
+    const maxSize = 320;
+    if (isTooLarge(email, maxSize)) return false;
+
+    const [local, domain] = email.split('@');
+
+    const maxLocalSize = 64;
+    if (isTooLarge(local, maxLocalSize)) return false;
+    const maxDomainSize = 255;
+    if (isTooLarge(domain, maxDomainSize)) return false;
+
+    const domainParts = domain.split('.');
+
+    const maxDomainPartsSize = 63;
+    const isDomainPartsTooLarge = domainParts
+      .reduce<boolean>((prev, current) => prev || isTooLarge(current, maxDomainPartsSize), false);
+    if (isDomainPartsTooLarge) return false;
+
+    return true;
+  }
+
   public static create(email: string): Either<InvalidEmailError, Email> {
-    if (isValid(email)) return success(new Email(email));
+    if (this.isValid(email)) return success(new Email(email));
     return error(new InvalidEmailError(email));
   }
 }

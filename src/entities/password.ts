7a471fb3ceb7adddd3aa6207dc8a651b70728919
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 22:37:26 -0300

    feat: ensure password validation

--- src/entities/password.ts
+++ src/entities/password.ts
@@ -1,5 +1,9 @@
-import { Either, success } from '@/shared';
+import { Either, success, error } from '@/shared';
 import { InvalidPasswordError } from '@/entities/errors/invalid-password-error';
+
+const isEmpty = (str: string): boolean => !str;
+const noNumberIn = (str: string) => !(/\d/.test(str));
+const isTooShort = (str: string) => str.length < 6;
 
 export class Password {
   readonly value: string;
@@ -9,7 +13,15 @@ export class Password {
     Object.freeze(this);
   }
 
+  public static isValid(password: string): boolean {
+    if (isEmpty(password)) return false;
+    if (noNumberIn(password)) return false;
+    if (isTooShort(password)) return false;
+    return true;
+  }
+
   public static create(password: string): Either<InvalidPasswordError, Password> {
-    return success(new Password(password));
+    if (this.isValid(password)) return success(new Password(password));
+    return error(new InvalidPasswordError(password));
   }
 }

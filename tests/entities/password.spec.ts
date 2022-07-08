0a90f3669f2ccce05ff9ad26955f05346acd1733
Victor Kayk <victorkayk77@gmail.com>
2022-07-07 22:37:35 -0300

    test: ensure password validation

--- tests/entities/password.spec.ts
+++ tests/entities/password.spec.ts
@@ -1,9 +1,28 @@
 import { Password } from '@/entities/password';
+import { InvalidPasswordError } from '@/entities/errors/invalid-password-error';
 
 describe('Password Entity', () => {
   it('Should create a password on success', () => {
-    const password = Password.create('any_password');
+    const password = Password.create('any_password_1');
     expect(password.isSuccess()).toBe(true);
-    expect(password.value).toEqual({ value: 'any_password' });
+    expect(password.value).toEqual({ value: 'any_password_1' });
+  });
+
+  it('Should return an error if password is empty', () => {
+    const password = Password.create('');
+    expect(password.isError()).toBe(true);
+    expect(password.value).toEqual(new InvalidPasswordError(''));
+  });
+
+  it('Should return an error if password has no number', () => {
+    const password = Password.create('any_password');
+    expect(password.isError()).toBe(true);
+    expect(password.value).toEqual(new InvalidPasswordError('any_password'));
+  });
+
+  it('Should return an error if password is too short', () => {
+    const password = Password.create('pass');
+    expect(password.isError()).toBe(true);
+    expect(password.value).toEqual(new InvalidPasswordError('pass'));
   });
 });

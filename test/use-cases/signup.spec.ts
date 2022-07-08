1c16e3fafd403f3027f352e04c0212443beea3e2
Victor Kayk <victorkayk77@gmail.com>
2022-07-08 01:30:26 -0300

    refactor: rename tests folder to test

---
+++ test/use-cases/signup.spec.ts
@@ -0,0 +1,17 @@
+type SutTypes = {
+  sut: ,
+}.
+
+const makeSut = (): SutTypes => {
+  const sut = new ();
+  return {
+    sut,
+  };
+};
+
+describe('SignUp Use Case', () => {
+  it('', () => {
+    const { sut } = makeSut();
+    ;
+  });
+});

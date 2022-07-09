cf0d305ca75a3abe06e95d3569b87bc1040c08b9
Victor Kayk <victorkayk77@gmail.com>
2022-07-09 17:43:39 -0300

    test: add required fields validation

---
+++ test/adapters/util/validations/required-fields-validation.spec.ts
@@ -0,0 +1,32 @@
+import { RequiredFieldsValidation } from '@/adapters/util/validations';
+import { MissingParamsError } from '@/adapters/errors';
+
+interface SutTypes {
+  sut: RequiredFieldsValidation
+}
+
+const makeSut = (): SutTypes => {
+  const requiredFieldsValidation = new RequiredFieldsValidation(['field', 'field1']);
+  return {
+    sut: requiredFieldsValidation,
+  };
+};
+
+describe('Required Fields validation', () => {
+  test('Should return a MissingParamsError if validation fails', () => {
+    const { sut } = makeSut();
+    const error = sut.validate({
+      invalid_field: 'invalid_field',
+    });
+    expect(error).toEqual(new MissingParamsError('field'));
+  });
+
+  test('Should return null if validation succeeds', () => {
+    const { sut } = makeSut();
+    const isValid = sut.validate({
+      field: 'field',
+      field1: 'field1',
+    });
+    expect(isValid).toBe(null);
+  });
+});

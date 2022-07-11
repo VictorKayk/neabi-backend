eac96573033811f57e4515ce31cd16ec10a127d0
Victor Kayk <victorkayk77@gmail.com>
2022-07-11 01:56:43 -0300

    test: userRepository prisma adapter

---
+++ test/infra/repositories/user-repository.spec.ts
@@ -0,0 +1,94 @@
+import { UserRepository } from '@/infra/repositories';
+import { UserBuilder } from '@/test/builders/user-builder';
+import prisma from '@/main/config/prisma';
+
+type SutTypes = {
+  sut: UserRepository,
+  user: UserBuilder,
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new UserRepository();
+  const user = new UserBuilder();
+  return {
+    sut,
+    user,
+  };
+};
+
+describe('User Repository Implementation', () => {
+  it('Should return an account on add success', async () => {
+    const { sut, user } = makeSut();
+
+    jest.spyOn(prisma.user, 'create').mockResolvedValue({
+      ...user.build(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const account = await sut.add(user.build());
+    expect(account.id).toBe(user.build().id);
+    expect(account.name).toBe(user.build().name);
+    expect(account.email).toBe(user.build().email);
+    expect(account.password).toBe(user.build().password);
+    expect(account.accessToken).toBe(user.build().accessToken);
+    expect(account.createdAt).toBeTruthy();
+    expect(account.updatedAt).toBeTruthy();
+  });
+
+  it('Should return an account on findByEmail success', async () => {
+    const { sut, user } = makeSut();
+
+    jest.spyOn(prisma.user, 'findFirst').mockResolvedValue({
+      ...user.build(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const account = await sut.findByEmail(user.build().email);
+    expect(account?.id).toBe(user.build().id);
+    expect(account?.name).toBe(user.build().name);
+    expect(account?.email).toBe(user.build().email);
+    expect(account?.password).toBe(user.build().password);
+    expect(account?.accessToken).toBe(user.build().accessToken);
+    expect(account?.createdAt).toBeTruthy();
+    expect(account?.updatedAt).toBeTruthy();
+  });
+
+  it('Should return null on findByEmail fails', async () => {
+    const { sut, user } = makeSut();
+
+    jest.spyOn(prisma.user, 'findFirst').mockResolvedValue(null);
+
+    const account = await sut.findByEmail(user.build().email);
+    expect(account).toBe(null);
+  });
+
+  it('Should return an account on findById success', async () => {
+    const { sut, user } = makeSut();
+
+    jest.spyOn(prisma.user, 'findFirst').mockResolvedValue({
+      ...user.build(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const account = await sut.findById(user.build().id);
+    expect(account?.id).toBe(user.build().id);
+    expect(account?.name).toBe(user.build().name);
+    expect(account?.email).toBe(user.build().email);
+    expect(account?.password).toBe(user.build().password);
+    expect(account?.accessToken).toBe(user.build().accessToken);
+    expect(account?.createdAt).toBeTruthy();
+    expect(account?.updatedAt).toBeTruthy();
+  });
+
+  it('Should return null on findById fails', async () => {
+    const { sut, user } = makeSut();
+
+    jest.spyOn(prisma.user, 'findFirst').mockResolvedValue(null);
+
+    const account = await sut.findById(user.build().id);
+    expect(account).toBe(null);
+  });
+});

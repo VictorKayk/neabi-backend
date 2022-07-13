d9dbd162b6a35ec3323dee86d8ec0cbd17e2d77b
Victor Kayk <victorkayk77@gmail.com>
2022-07-13 20:26:27 -0300

    refactor: update server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,7 @@
 import 'module-alias/register';
+import app from '@/main/config/app';
+import env from '@/main/config/env';
+
+app.listen(env.port, () => {
+  console.log(`Listen on port: ${env.port}`);
+});

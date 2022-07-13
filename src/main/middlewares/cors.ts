f696cf520576f9c27620c6e55d96235828c73982
Victor Kayk <victorkayk77@gmail.com>
2022-07-13 20:21:52 -0300

    feat: add middlewares

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express'
+
+export function cors(req: Request, res: Response, next: NextFunction): void {
+  res.set('access-control-allow-origin', '*')
+    .set('access-control-allow-methods', '*')
+    .set('access-control-allow-headers', '*')
+  next()
+}

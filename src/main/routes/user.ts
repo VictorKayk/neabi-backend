d7c74d7690a7ec69412b269ba03dd933221c3506
Victor Kayk <victorkayk77@gmail.com>
2022-07-14 21:21:42 -0300

    feat: sign in route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
 import { makeSignUpController } from '@/main/factories/sign-up';
+import { makeSignInController } from '@/main/factories/sign-in';
 
 export function user(router: Router) {
   router.post('/signup', routerAdapter(makeSignUpController()));
+  router.post('/signin', routerAdapter(makeSignInController()));
 }

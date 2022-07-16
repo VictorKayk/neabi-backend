db5ff6bc55442c4510006be54e5da924deb8171a
Victor Kayk <victorkayk77@gmail.com>
2022-07-16 00:24:49 -0300

    feat: add decrypt to jwt adapter

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,12 +1,23 @@
 import jwt from 'jsonwebtoken';
-import { IEncrypter } from '@/use-cases/interfaces';
+import { IEncrypter, IDecrypter, IPayload } from '@/use-cases/interfaces';
+import { Either, success, error } from '@/shared';
+import { UnauthorizedError } from '@/use-cases/errors';
 
-export class JwtAdapter implements IEncrypter {
+export class JwtAdapter implements IEncrypter, IDecrypter {
   constructor(private readonly secret: string) { }
 
   async encrypt(value: string): Promise<string> {
     return jwt.sign({
       id: value,
     }, this.secret);
+  }
+
+  async decrypt(value: string): Promise<Either<UnauthorizedError, IPayload>> {
+    try {
+      const payload = jwt.verify(value, this.secret) as IPayload;
+      return success(payload);
+    } catch (e) {
+      return error(new UnauthorizedError());
+    }
   }
 }

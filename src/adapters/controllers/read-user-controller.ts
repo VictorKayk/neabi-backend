7f52a37083895249a1558ae20c469c6cf1bdaa1f
Victor Kayk <victorkayk77@gmail.com>
2022-07-17 21:22:26 -0300

    feat: add ReadUserController

---
+++ src/adapters/controllers/read-user-controller.ts
@@ -0,0 +1,26 @@
+import { ReadUserUseCase } from '@/use-cases/read-user';
+import { IController, IHttpRequest, IHttpResponse } from '@/adapters/interfaces';
+import { ok, serverError, unauthorized } from '@/adapters/util/http';
+import { IUserRepositoryData } from '@/use-cases/interfaces';
+
+export class ReadUserController implements IController {
+  constructor(
+    private readonly readUserUseCase: ReadUserUseCase,
+  ) { }
+
+  async handle({ body }: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      const { id } = body;
+
+      const accountOrError = await this.readUserUseCase.execute(id);
+      if (accountOrError.isError()) {
+        return unauthorized(accountOrError.value);
+      }
+
+      const account: IUserRepositoryData = accountOrError.value;
+      return ok(account);
+    } catch (error) {
+      return serverError(error as Error);
+    }
+  }
+}

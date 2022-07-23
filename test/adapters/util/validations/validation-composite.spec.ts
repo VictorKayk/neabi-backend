80c216872132a82f5ca4a18c7e7cdb8d5e144f46
Victor Kayk <victorkayk77@gmail.com>
2022-07-23 17:48:05 -0300

    refactor: improve validation composite test code

--- test/adapters/util/validations/validation-composite.spec.ts
+++ test/adapters/util/validations/validation-composite.spec.ts
@@ -9,11 +9,9 @@ interface SutTypes {
 }
 
 const makeSut = (): SutTypes => {
-  const validations = [
-    makeValidation(),
-    makeValidation(),
-  ];
+  const validations = [makeValidation(), makeValidation()];
   const sut = new ValidationComposite(validations);
+
   return {
     sut,
     validations,
@@ -24,27 +22,23 @@ describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
     const { sut, validations } = makeSut();
     jest.spyOn(validations[1], 'validate').mockReturnValueOnce(new InvalidParamsError('field'));
-    const isValid = sut.validate({
-      field: 'any_field',
-    });
+    const isValid = sut.validate({ field: 'any_field' });
     expect(isValid).toEqual(new InvalidParamsError('field'));
   });
 
   it('Should return the first error if more then one validation fails', () => {
     const { sut, validations } = makeSut();
+
     jest.spyOn(validations[0], 'validate').mockReturnValueOnce(new InvalidParamsError('field'));
     jest.spyOn(validations[1], 'validate').mockReturnValueOnce(new MissingParamsError('field'));
-    const isValid = sut.validate({
-      field: 'any_field',
-    });
+
+    const isValid = sut.validate({ field: 'any_field' });
     expect(isValid).toEqual(new InvalidParamsError('field'));
   });
 
   it('Should return null if validation succeed', () => {
     const { sut } = makeSut();
-    const isValid = sut.validate({
-      field: 'any_field',
-    });
+    const isValid = sut.validate({ field: 'any_field' });
     expect(isValid).toBe(null);
   });
 });

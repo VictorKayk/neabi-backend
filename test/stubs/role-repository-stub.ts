46a8056d844a820e75d26a86606c819eae19ff09
Victor Kayk <victorkayk77@gmail.com>
2022-07-30 18:38:33 -0300

    feat: add role stubs

---
+++ test/stubs/role-repository-stub.ts
@@ -0,0 +1,25 @@
+import {
+  IRoleRepository,
+  IRoleData,
+  IRoleRepositoryReturnData,
+} from '@/use-cases/roles/interfaces';
+
+export const makeRoleRepository = (): IRoleRepository => {
+  class RoleRepositoryStub implements IRoleRepository {
+    async findByRole(role: string): Promise<IRoleRepositoryReturnData | null> {
+      return null;
+    }
+
+    async findById(id: string): Promise<IRoleRepositoryReturnData | null> {
+      return null;
+    }
+
+    async add(userData: IRoleData): Promise<IRoleRepositoryReturnData> {
+      return {
+        ...userData,
+        createdAt: new Date(),
+      };
+    }
+  }
+  return new RoleRepositoryStub();
+};

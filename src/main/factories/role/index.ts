9f50efdbc08c3635c3cd95e0a277f1e8c2dc3e02
Victor Kayk <victorkayk77@gmail.com>
2022-07-31 21:13:47 -0300

    feat: add ReadAllRoles route

--- src/main/factories/role/index.ts
+++ src/main/factories/role/index.ts
@@ -1,2 +1,3 @@
 export * from '@/main/factories/role/create-role';
 export * from '@/main/factories/role/read-role-by-id';
+export * from '@/main/factories/role/read-all-roles';
--- src/main/routes/role-routes.ts
+++ src/main/routes/role-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeCreateRoleController, makeReadRoleByIdController } from '@/main/factories/role';
+import { makeCreateRoleController, makeReadRoleByIdController, makeReadAllRolesController } from '@/main/factories/role';
 import { authentication } from '@/main/middlewares/authentication';
 
 export function role(router: Router) {
   router.post('/role', authentication, routerAdapter(makeCreateRoleController()));
+  router.get('/role/all', authentication, routerAdapter(makeReadAllRolesController()));
   router.get('/role/:id', authentication, routerAdapter(makeReadRoleByIdController()));
 }

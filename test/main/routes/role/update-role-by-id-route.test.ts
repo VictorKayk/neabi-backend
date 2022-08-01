a0193f997fe98ab8e1f2903be32660e8f5484d7c
Victor Kayk <victorkayk77@gmail.com>
2022-08-01 01:36:00 -0300

    feat: add UpdateRoleById route test

---
+++ test/main/routes/role/update-role-by-id-route.test.ts
@@ -0,0 +1,84 @@
+import request from 'supertest';
+import app from '@/main/config/app';
+import prisma from '@/main/config/prisma';
+import { UserBuilder } from '@/test/builders/user-builder';
+
+jest.mock('@/test/mocks/prisma-client.ts');
+
+// Authentication
+jest.mock('bcrypt', () => ({
+  async compare(hash: string, value: string): Promise<boolean> {
+    return new Promise((resolve) => resolve(true));
+  },
+}));
+
+jest.mock('jsonwebtoken', () => ({
+  verify() {
+    return { id: 'any_id' };
+  },
+}));
+
+jest.spyOn(prisma.user, 'findFirst').mockResolvedValue({
+  ...new UserBuilder().build(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+});
+
+describe('UpdateRoleById Route', () => {
+  it('Should return 200 on update role by id route success', async () => {
+    jest.spyOn(prisma.roles, 'findFirst')
+      .mockResolvedValue({
+        id: 'any_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      });
+
+    jest.spyOn(prisma.roles, 'update')
+      .mockResolvedValue({
+        id: 'any_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      });
+
+    await request(app).patch('/api/role/any_id')
+      .set('x-access-token', 'any_encrypted_string')
+      .send({ role: 'new_role' })
+      .expect(200);
+  });
+
+  it('Should return 400 on update role by id route if params are invalid', async () => {
+    jest.spyOn(prisma.roles, 'findFirst')
+      .mockResolvedValue({
+        id: 'any_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      });
+
+    await request(app).patch('/api/role/any_id')
+      .set('x-access-token', 'any_encrypted_string')
+      .send({ role: '' })
+      .expect(400);
+  });
+
+  it('Should return 401 on update role by id route if params are invalid', async () => {
+    jest.spyOn(prisma.roles, 'findFirst')
+      .mockResolvedValueOnce({
+        id: 'any_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      })
+      .mockResolvedValueOnce({
+        id: 'invalid_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      });
+
+    await request(app).patch('/api/role/any_id')
+      .set('x-access-token', 'any_encrypted_string')
+      .send({ role: 'any_role' })
+      .expect(401);
+  });
+
+  it('Should return 500 if update role by id route throws', async () => {
+    jest.spyOn(prisma.roles, 'findFirst')
+      .mockResolvedValue({
+        id: 'any_id', role: 'any_role', createdAt: new Date(), updatedAt: new Date(),
+      });
+    jest.spyOn(prisma.roles, 'update').mockImplementationOnce(() => { throw new Error(); });
+
+    await request(app).patch('/api/role/any_id')
+      .set('x-access-token', 'any_encrypted_string')
+      .send({ role: 'any_role' })
+      .expect(500);
+  });
+});

890c3aa77e03df04d74fb13bf0d32abf95e2c78a
Victor Kayk <victorkayk77@gmail.com>
2022-08-02 18:38:38 -0300

    refactor: return UserHasRoles in all UserRepository methods

--- src/infra/repositories/user-repository.ts
+++ src/infra/repositories/user-repository.ts
@@ -10,24 +10,23 @@ export class UserRepository implements IUserRepository {
   async add(userData: IUserRepositoryData): Promise<IUserRepositoryReturnData> {
     const account = await prisma.user.create({
       data: userData,
+      include: { UserHasRoles: true },
     });
     return account;
   }
 
   async findByEmail(email: string): Promise<IUserRepositoryReturnData | null> {
     const account = prisma.user.findFirst({
-      where: {
-        email,
-      },
+      where: { email },
+      include: { UserHasRoles: true },
     });
     return account;
   }
 
   async findById(id: string): Promise<IUserRepositoryReturnData | null> {
     const account = prisma.user.findFirst({
-      where: {
-        id,
-      },
+      where: { id },
+      include: { UserHasRoles: true },
     });
     return account;
   }
@@ -37,6 +36,7 @@ export class UserRepository implements IUserRepository {
     const user = prisma.user.update({
       where: { email },
       data: userData,
+      include: { UserHasRoles: true },
     });
     return user;
   }
@@ -46,6 +46,7 @@ export class UserRepository implements IUserRepository {
     const user = prisma.user.update({
       where: { id },
       data: userData,
+      include: { UserHasRoles: true },
     });
     return user;
   }
@@ -53,12 +54,15 @@ export class UserRepository implements IUserRepository {
   async deleteById(id: string): Promise<IUserRepositoryReturnData> {
     const user = prisma.user.delete({
       where: { id },
+      include: { UserHasRoles: true },
     });
     return user;
   }
 
   async readAllUsers(): Promise<IUserRepositoryReturnData[] | []> {
-    const user = prisma.user.findMany();
+    const user = prisma.user.findMany({
+      include: { UserHasRoles: true },
+    });
     return user;
   }
 }

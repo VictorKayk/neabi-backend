62009f6e255c1f9a87152859a7c493781ff8f9c1
Victor Kayk <victorkayk77@gmail.com>
2022-08-02 09:23:21 -0300

    refactor: add roles to user

--- src/use-cases/user/interfaces/user-repository-return-data-interface.ts
+++ src/use-cases/user/interfaces/user-repository-return-data-interface.ts
@@ -1,8 +1,10 @@
 import { IUserData } from '@/use-cases/user/interfaces';
+import { IRoleRepositoryReturnData } from '@/use-cases/role/interfaces';
 
 export interface IUserRepositoryReturnData extends IUserData {
   id: string,
   accessToken: string,
   createdAt: Date,
   updatedAt: Date,
+  roles?: [] | Array<IRoleRepositoryReturnData>
 }

2dbe0e5d245879d7fa763b104ccbae778a11b3b5
Victor Kayk <victorkayk77@gmail.com>
2022-08-03 10:45:46 -0300

    feat: add id params validation to ReadRoleById

--- test/adapters/controllers/role/read-role-by-id/read-role-by-id-controller.spec.ts
+++ test/adapters/controllers/role/read-role-by-id/read-role-by-id-controller.spec.ts
@@ -1,22 +1,27 @@
 import { ReadRoleByIdUseCase } from '@/use-cases/role/read-role-by-id';
 import { NonExistingRoleError } from '@/use-cases/role/errors';
 import { ServerError } from '@/adapters/errors';
-import { serverError, unauthorized } from '@/adapters/util/http';
-import { makeFakeRequestAuthenticated, makeReadRoleByIdUseCase } from '@/test/stubs';
+import { badRequest, serverError, unauthorized } from '@/adapters/util/http';
+import { makeFakeRequestAuthenticated, makeReadRoleByIdUseCase, makeValidation } from '@/test/stubs';
 import { error, success } from '@/shared';
 import { ReadRoleByIdController } from '@/adapters/controllers/role/read-role-by-id';
+import { IValidation } from '@/adapters/controllers/interfaces';
+import { MissingParamsError } from '@/adapters/controllers/errors/missing-params-error';
 
 type SutTypes = {
   sut: ReadRoleByIdController,
+  validation: IValidation,
   useCase: ReadRoleByIdUseCase,
 };
 
 const makeSut = (): SutTypes => {
+  const validation = makeValidation();
   const useCase = makeReadRoleByIdUseCase();
-  const sut = new ReadRoleByIdController(useCase);
+  const sut = new ReadRoleByIdController(validation, useCase);
 
   return {
     sut,
+    validation,
     useCase,
   };
 };
@@ -77,5 +82,19 @@ describe('ReadRoleById Controller ', () => {
     });
 
     expect(response).toEqual(unauthorized(new NonExistingRoleError()));
+  });
+
+  it('Should call Validation with correct values', async () => {
+    const { sut, validation } = makeSut();
+    const validationSpy = jest.spyOn(validation, 'validate');
+    await sut.handle({ ...makeFakeRequestAuthenticated(), params: { id: 'any_id' } });
+    expect(validationSpy).toHaveBeenCalledWith({ id: 'any_id' });
+  });
+
+  it('Should return 400 if Validation returns an error', async () => {
+    const { sut, validation } = makeSut();
+    jest.spyOn(validation, 'validate').mockReturnValueOnce(new MissingParamsError('any_field'));
+    const response = await sut.handle({ ...makeFakeRequestAuthenticated(), params: { id: 'any_id' } });
+    expect(response).toEqual(badRequest(new MissingParamsError('any_field')));
   });
 });

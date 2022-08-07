c00adf4b7a7fba58d76d00c5c6579dfe5c34b5c5
Victor Kayk <victorkayk77@gmail.com>
2022-08-07 19:19:28 -0300

    feat: add eReadAllRolesFromUserController factory

---
+++ src/main/factories/user-has-role/read-all-roles-from-user/index.ts
@@ -0,0 +1,2 @@
+export * from '@/main/factories/user-has-role/read-all-roles-from-user/read-all-roles-from-user-validation-factory';
+export * from '@/main/factories/user-has-role/read-all-roles-from-user/read-all-roles-from-user-factory';

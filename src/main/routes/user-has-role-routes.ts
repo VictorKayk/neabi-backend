2407dae2e4793ed725385b087490de9384714c8d
Victor Kayk <victorkayk77@gmail.com>
2022-08-07 19:31:11 -0300

    feat: add ReadAllRolesFromUser route

--- src/main/routes/user-has-role-routes.ts
+++ src/main/routes/user-has-role-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeAddRoleToUserController, makeRemoveRoleFromUserController } from '@/main/factories/user-has-role';
+import { makeReadAllRolesFromUserController, makeAddRoleToUserController, makeRemoveRoleFromUserController } from '@/main/factories/user-has-role';
 import { authentication } from '@/main/middlewares/authentication';
 
 export function userHasRole(router: Router) {
+  router.get('/user/:userId/role/all', authentication, routerAdapter(makeReadAllRolesFromUserController()));
   router.post('/user/:userId/role/:roleId', authentication, routerAdapter(makeAddRoleToUserController()));
   router.delete('/user/:userId/role/:roleId', authentication, routerAdapter(makeRemoveRoleFromUserController()));
 }

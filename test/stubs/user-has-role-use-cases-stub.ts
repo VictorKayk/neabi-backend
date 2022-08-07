7c9580366e2265997cde232368e30ae67bb970f9
Victor Kayk <victorkayk77@gmail.com>
2022-08-07 01:04:39 -0300

    feat: add RemoveRoleFromUserController test

--- test/stubs/user-has-role-use-cases-stub.ts
+++ test/stubs/user-has-role-use-cases-stub.ts
@@ -1,7 +1,13 @@
 import { AddRoleToUserUseCase } from '@/use-cases/user-has-role/add-role-to-user';
+import { RemoveRoleFromUserUseCase } from '@/use-cases/user-has-role/remove-role-from-user';
 import { makeUserHasRoleRepository } from '@/test/stubs';
 
 export const makeAddRoleToUserUseCase = (): AddRoleToUserUseCase => {
   const userHasRoleRepository = makeUserHasRoleRepository();
   return new AddRoleToUserUseCase(userHasRoleRepository);
+};
+
+export const makeRemoveRoleFromUserUseCase = (): RemoveRoleFromUserUseCase => {
+  const userHasRoleRepository = makeUserHasRoleRepository();
+  return new RemoveRoleFromUserUseCase(userHasRoleRepository);
 };

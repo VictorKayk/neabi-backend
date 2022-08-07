e63809f172d87eaf8d25d1184298401edadad8b9
Victor Kayk <victorkayk77@gmail.com>
2022-08-07 17:49:40 -0300

    feat: add ReadAllRolesFromUserController test

--- test/stubs/user-has-role-use-cases-stub.ts
+++ test/stubs/user-has-role-use-cases-stub.ts
@@ -1,5 +1,6 @@
 import { AddRoleToUserUseCase } from '@/use-cases/user-has-role/add-role-to-user';
 import { RemoveRoleFromUserUseCase } from '@/use-cases/user-has-role/remove-role-from-user';
+import { ReadAllRolesFromUserUseCase } from '@/use-cases/user-has-role/read-all-roles-from-user';
 import { makeUserHasRoleRepository } from '@/test/stubs';
 
 export const makeAddRoleToUserUseCase = (): AddRoleToUserUseCase => {
@@ -10,4 +11,9 @@ export const makeAddRoleToUserUseCase = (): AddRoleToUserUseCase => {
 export const makeRemoveRoleFromUserUseCase = (): RemoveRoleFromUserUseCase => {
   const userHasRoleRepository = makeUserHasRoleRepository();
   return new RemoveRoleFromUserUseCase(userHasRoleRepository);
+};
+
+export const makeReadAllRolesFromUserUseCase = (): ReadAllRolesFromUserUseCase => {
+  const userHasRoleRepository = makeUserHasRoleRepository();
+  return new ReadAllRolesFromUserUseCase(userHasRoleRepository);
 };

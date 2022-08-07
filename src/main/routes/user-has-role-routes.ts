ad29b1a3c6539a6f9865c4b83ab8edf357213b2e
Victor Kayk <victorkayk77@gmail.com>
2022-08-07 13:02:41 -0300

    feat: add RemoveRoleFromUser role

--- src/main/routes/user-has-role-routes.ts
+++ src/main/routes/user-has-role-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeAddRoleToUserController } from '@/main/factories/user-has-role';
+import { makeAddRoleToUserController, makeRemoveRoleFromUserController } from '@/main/factories/user-has-role';
 import { authentication } from '@/main/middlewares/authentication';
 
 export function userHasRole(router: Router) {
   router.post('/user/:userId/role/:roleId', authentication, routerAdapter(makeAddRoleToUserController()));
+  router.delete('/user/:userId/role/:roleId', authentication, routerAdapter(makeRemoveRoleFromUserController()));
 }

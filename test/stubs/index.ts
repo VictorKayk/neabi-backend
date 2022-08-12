e972eda529427ad1432188ec175992e1421ebad7
Victor Kayk <victorkayk77@gmail.com>
2022-08-12 00:05:09 -0300

    refactor: rename IdGenerator to UniversallyUniqueIdentifierGenerator

--- test/stubs/index.ts
+++ test/stubs/index.ts
@@ -1,7 +1,7 @@
 export * from '@/test/stubs/encrypter-decrypter-stub';
 export * from '@/test/stubs/fake-request-stub';
 export * from '@/test/stubs/hash-stub';
-export * from '@/test/stubs/id-generator-stub';
+export * from '@/test/stubs/universally-unique-identifier-generator-stub';
 export * from '@/test/stubs/user-use-cases-stub';
 export * from '@/test/stubs/user-repository-stub';
 export * from '@/test/stubs/role-repository-stub';
@@ -9,3 +9,4 @@ export * from '@/test/stubs/role-use-cases-stub';
 export * from '@/test/stubs/user-has-role-repository-stub';
 export * from '@/test/stubs/user-has-role-use-cases-stub';
 export * from '@/test/stubs/validation-stub';
+export * from '@/test/stubs/email-validation-token-repository-stub';

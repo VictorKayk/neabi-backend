5485e48669b6185ae71cb2f3eb32030afb749285
Victor Kayk <victorkayk77@gmail.com>
2022-08-12 01:21:40 -0300

    refactor: add addEmailVerificationTokenUseCase to makeSignUpController

--- src/main/factories/user/sign-up/sign-up-factory.ts
+++ src/main/factories/user/sign-up/sign-up-factory.ts
@@ -2,10 +2,11 @@ import { SignUpUseCase } from '@/use-cases/user/sign-up';
 import { SignUpController } from '@/adapters/controllers/user/sign-up';
 import { IController } from '@/adapters/controllers/interfaces';
 import { UuidAdapter } from '@/infra/universally-unique-identifier';
-import { UserRepository } from '@/infra/repositories';
+import { EmailVerificationTokenRepository, UserRepository } from '@/infra/repositories';
 import { BcryptAdapter, JwtAdapter } from '@/infra/criptography';
 import { makeSignUpValidationFactory } from '@/main/factories/user';
 import env from '@/main/config/env';
+import { AddEmailVerificationTokenUseCase } from '@/use-cases/email-verification-token/add-email-verification-token';
 
 export function makeSignUpController(): IController {
   const salt = env.bcryptSalt;
@@ -13,7 +14,16 @@ export function makeSignUpController(): IController {
   const bcryptAdapter = new BcryptAdapter(salt);
   const uuidAdapter = new UuidAdapter();
   const jwtAdapter = new JwtAdapter(env.jwtSecret, env.expiresIn);
+
   const signUpUseCase = new SignUpUseCase(userRepository, bcryptAdapter, uuidAdapter, jwtAdapter);
-  const signUpController = new SignUpController(makeSignUpValidationFactory(), signUpUseCase);
+
+  const emailVerificationTokenRepository = new EmailVerificationTokenRepository();
+  const addEmailVerificationTokenUseCase = new AddEmailVerificationTokenUseCase(
+    emailVerificationTokenRepository, uuidAdapter,
+  );
+
+  const signUpController = new SignUpController(
+    makeSignUpValidationFactory(), signUpUseCase, addEmailVerificationTokenUseCase,
+  );
   return signUpController;
 }

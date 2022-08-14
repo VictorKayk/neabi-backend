8b1545cd0351c3ef7fa3d43f618287daf5f173c6
Victor Kayk <victorkayk77@gmail.com>
2022-08-14 01:50:58 -0300

    fix: import in IEmailService

--- src/use-cases/interfaces/email-service-interface.ts
+++ src/use-cases/interfaces/email-service-interface.ts
@@ -1,4 +1,4 @@
-import { IEmailOptions } from '@/use-cases/email-service/interfaces';
+import { IEmailOptions } from '@/use-cases/interfaces';
 
 export interface IEmailService {
   send(emailOptions: IEmailOptions): Promise<null>

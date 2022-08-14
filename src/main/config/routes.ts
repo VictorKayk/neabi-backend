1a9ac94c5cf774d1570b81b2d3e08e285286f213
Victor Kayk <victorkayk77@gmail.com>
2022-08-14 00:45:09 -0300

    feat: add SendVerificationTokenToUserRoute

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,13 @@
 import { Express, Router } from 'express';
-import { user, role, userHasRole } from '@/main/routes';
+import {
+  user, role, userHasRole, sendVerificationTokenToUser,
+} from '@/main/routes';
 
 export default (app: Express): void => {
   const router = Router();
   app.use('/api', router);
-  user(router);
-  role(router);
+  sendVerificationTokenToUser(router);
   userHasRole(router);
+  role(router);
+  user(router);
 };

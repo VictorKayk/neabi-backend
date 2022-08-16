aa6be3cee299a8241921f976bb6f25fe2d412cf5
Victor Kayk <victorkayk77@gmail.com>
2022-08-15 21:41:44 -0300

    feat: add verify token route

--- src/main/routes/verification-token-routes.ts
+++ src/main/routes/verification-token-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeSendVerificationTokenToUserController } from '@/main/factories/verification-token';
+import { makeSendVerificationTokenToUserController, makeVerifyTokenController } from '@/main/factories/verification-token';
 import { authentication } from '@/main/middlewares/authentication';
 
 export function sendVerificationTokenToUser(router: Router) {
   router.get('/user/verification/token', authentication, routerAdapter(makeSendVerificationTokenToUserController()));
+  router.get('/user/:userId/verification/token/:token', authentication, routerAdapter(makeVerifyTokenController()));
 }

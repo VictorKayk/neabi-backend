fe3ce47f2b1b739de6d37143528226650a3f79e6
Victor Kayk <victorkayk77@gmail.com>
2022-08-16 23:32:06 -0300

    feat: add bcryptAdapter to makeVerifyTokenController

--- src/main/factories/verification-token/verify-token/verify-token-factory.ts
+++ src/main/factories/verification-token/verify-token/verify-token-factory.ts
@@ -3,10 +3,14 @@ import { IController } from '@/adapters/controllers/interfaces';
 import { VerificationTokenRepository } from '@/infra/repositories';
 import { makeVerifyTokenValidationFactory } from '@/main/factories/verification-token';
 import { VerifyTokenUseCase } from '@/use-cases/verification-token/verify-token';
+import { BcryptAdapter } from '@/infra/criptography';
+import env from '@/main/config/env';
 
 export function makeVerifyTokenController(): IController {
   const verificationTokenRepository = new VerificationTokenRepository();
-  const verifyTokenUseCase = new VerifyTokenUseCase(verificationTokenRepository);
+  const salt = env.bcryptSalt;
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const verifyTokenUseCase = new VerifyTokenUseCase(verificationTokenRepository, bcryptAdapter);
 
   const verifyTokenController = new VerifyTokenController(
     makeVerifyTokenValidationFactory(),

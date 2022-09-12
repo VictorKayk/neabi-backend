75f505b5af167637ed7ed7a07f7f35ee9d6246f1
Victor Kayk <victorkayk77@gmail.com>
2022-09-11 23:40:02 -0300

    refactor: improve test stubs

--- test/stubs/verification-token-use-cases-stub.ts
+++ test/stubs/verification-token-use-cases-stub.ts
@@ -3,10 +3,11 @@ import { AddVerificationTokenUseCase } from '@/use-cases/verification-token/add-
 
 export const makeAddVerificationTokenUseCase = (): AddVerificationTokenUseCase => {
   const VerificationTokenRepositoryStub = makeVerificationTokenRepository();
+  const idGeneratorStub = makeUniversallyUniqueIdentifierGenerator();
   const tokenGeneratorStub = makeUniversallyUniqueIdentifierGenerator();
   const hasher = makeHasher();
   const AddVerificationTokenUseCaseStub = new AddVerificationTokenUseCase(
-    VerificationTokenRepositoryStub, tokenGeneratorStub, hasher,
+    VerificationTokenRepositoryStub, idGeneratorStub, tokenGeneratorStub, hasher,
   );
   return AddVerificationTokenUseCaseStub;
 };

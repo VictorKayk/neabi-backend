bb9556a66ed7da721279333155429c9e32a5e552
Victor Kayk <victorkayk77@gmail.com>
2022-09-14 12:15:43 -0300

    feat: add reset user password token repository

--- src/infra/repositories/index.ts
+++ src/infra/repositories/index.ts
@@ -2,3 +2,4 @@ export * from '@/infra/repositories/user-repository';
 export * from '@/infra/repositories/role-repository';
 export * from '@/infra/repositories/user-has-role-repository';
 export * from '@/infra/repositories/verification-token-repository';
+export * from '@/infra/repositories/reset-user-password-token-repository';

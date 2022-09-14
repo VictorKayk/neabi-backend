2fa5fb99415f23ff06f7d0459f1d7d608c076d1e
Victor Kayk <victorkayk77@gmail.com>
2022-09-14 12:26:42 -0300

    fix: add correct factory to send verification token to user validation factory

--- test/main/factories/verification-token/send-verification-token-to-user/send-verification-token-to-user-validation-factory.spec.ts
+++ test/main/factories/verification-token/send-verification-token-to-user/send-verification-token-to-user-validation-factory.spec.ts
@@ -1,11 +1,11 @@
-import { makeDeleteUserByIdValidationFactory } from '@/main/factories/user';
+import { makeSendVerificationTokenToUserController } from '@/main/factories/verification-token';
 import { ValidationComposite, RequiredFieldsValidation } from '@/adapters/controllers/utils/validations';
 
 jest.mock('@/adapters/controllers/utils/validations/validation-composite');
 
 describe('SendVerificationTokenToUser validation Factory', () => {
   it('Should call ValidationComposite with all validations', () => {
-    makeDeleteUserByIdValidationFactory();
+    makeSendVerificationTokenToUserController();
     expect(ValidationComposite).toHaveBeenCalledWith([
       new RequiredFieldsValidation(['id']),
     ]);

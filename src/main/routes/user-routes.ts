beb18a96ce5964e9f33dfe652cb930fa0d96bf0f
Victor Kayk <victorkayk77@gmail.com>
2022-09-17 15:38:54 -0300

    feat: add authorization to user routes

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -14,17 +14,18 @@ import {
   makeUpdateUserController,
 } from '@/main/factories/user';
 import { authentication, googleLoginAuth, googleLoginAuthCb } from '@/main/middlewares/authentication';
+import { authorization } from '@/main/middlewares/authorization';
 
 export function user(router: Router) {
   router.post('/signup', routerAdapter(makeSignUpController()));
   router.post('/signin', routerAdapter(makeSignInController()));
   router.get('/user/google', googleLoginAuth);
   router.get('/user/google/auth', googleLoginAuthCb, routerAdapter(makeExternalSignInController()));
-  router.get('/user', authentication, routerAdapter(makeReadUserController()));
-  router.get('/user/all', authentication, routerAdapter(makeAllReadUsersController()));
-  router.get('/user/:id', authentication, routerAdapter(makeReadUserByIdController()));
-  router.patch('/user', authentication, routerAdapter(makeUpdateUserController()));
-  router.patch('/user/:id', authentication, routerAdapter(makeUpdateUserByIdController()));
-  router.delete('/user', authentication, routerAdapter(makeDeleteUserController()));
-  router.delete('/user/:id', authentication, routerAdapter(makeDeleteUserByIdController()));
+  router.get('/user', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadUserController()));
+  router.get('/user/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeAllReadUsersController()));
+  router.get('/user/:id', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadUserByIdController()));
+  router.patch('/user', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeUpdateUserController()));
+  router.patch('/user/:id', authentication, authorization(['moderator', 'admin']), routerAdapter(makeUpdateUserByIdController()));
+  router.delete('/user', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeDeleteUserController()));
+  router.delete('/user/:id', authentication, authorization(['moderator', 'admin']), routerAdapter(makeDeleteUserByIdController()));
 }

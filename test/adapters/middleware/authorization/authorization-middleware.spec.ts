e81d405059aeda490599af39788d3f6d34655632
Victor Kayk <victorkayk77@gmail.com>
2022-09-17 01:36:24 -0300

    refactor: add userId in middlewareAdapter test

--- test/adapters/middleware/authorization/authorization-middleware.spec.ts
+++ test/adapters/middleware/authorization/authorization-middleware.spec.ts
@@ -13,7 +13,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
   const useCase = makeAuthorizationUseCase();
-  const sut = new AuthorizationMiddleware(useCase);
+  const sut = new AuthorizationMiddleware(useCase, ['any_role']);
 
   jest.spyOn(useCase, 'execute').mockResolvedValue(success(null));
 
@@ -27,8 +27,8 @@ describe('Authorization Middleware', () => {
   it('Should call AuthorizationUseCase with correct values', async () => {
     const { sut, useCase } = makeSut();
     const useCaseSpy = jest.spyOn(useCase, 'execute');
-    await sut.handle({ userId: 'any_userId', allowedRoles: ['any_role'] });
-    expect(useCaseSpy).toHaveBeenCalledWith({ allowedRoles: ['any_role'], userId: 'any_userId' });
+    await sut.handle({ userId: 'any_userId' });
+    expect(useCaseSpy).toHaveBeenCalledWith({ userId: 'any_userId', allowedRoles: ['any_role'] });
   });
 
   it('Should return 500 if use case throws', async () => {
@@ -36,20 +36,20 @@ describe('Authorization Middleware', () => {
 
     jest.spyOn(useCase, 'execute').mockImplementationOnce(() => { throw new Error(); });
 
-    const response = await sut.handle({ userId: 'any_userId', allowedRoles: ['any_role'] });
+    const response = await sut.handle({ userId: 'any_userId' });
     expect(response).toEqual(serverError(new ServerError()));
   });
 
   it('Should return 200 on success', async () => {
     const { sut } = makeSut();
-    const response = await sut.handle({ userId: 'any_userId', allowedRoles: ['any_role'] });
+    const response = await sut.handle({ userId: 'any_userId' });
     expect(response.statusCode).toBe(200);
   });
 
   it('Should return 401 if call AuthorizationUseCase with incorrect values', async () => {
     const { sut, useCase } = makeSut();
     jest.spyOn(useCase, 'execute').mockResolvedValue(error(new UnauthorizedError()));
-    const response = await sut.handle({ userId: 'invalid_userId', allowedRoles: ['any_role'] });
+    const response = await sut.handle({ userId: 'invalid_userId' });
     expect(response).toEqual(unauthorized(new UnauthorizedError()));
   });
 });

6a57b1aac519f7cd5542b67d23ba8739582910c7
Victor Kayk <victorkayk77@gmail.com>
2022-09-18 19:18:23 -0300

    feat: add no cache middleware test

---
+++ test/main/middlewares/no-cache.test.ts
@@ -0,0 +1,25 @@
+import setupApp from '@/main/config/app';
+import { noCache } from '@/main/middlewares';
+
+import { Express } from 'express';
+import request from 'supertest';
+
+let app: Express;
+
+describe('NoCache Middleware', () => {
+  beforeAll(async () => {
+    app = await setupApp();
+  });
+
+  test('Should disable cache', async () => {
+    app.get('/test_no_cache', noCache, (req, res) => {
+      res.send();
+    });
+    await request(app)
+      .get('/test_no_cache')
+      .expect('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('pragma', 'no-cache')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store');
+  });
+});

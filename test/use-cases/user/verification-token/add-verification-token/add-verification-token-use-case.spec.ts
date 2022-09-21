cd23a75bdbf04525716b99c78ad0d6a8732f9a5d
Victor Kayk <victorkayk77@gmail.com>
2022-09-21 00:26:12 -0300

    fix: add verification token use case test

--- test/use-cases/user/verification-token/add-verification-token/add-verification-token-use-case.spec.ts
+++ test/use-cases/user/verification-token/add-verification-token/add-verification-token-use-case.spec.ts
@@ -34,6 +34,7 @@ const makeSut = (): SutTypes => {
     isVerified: false,
     roles: [],
   });
+  jest.spyOn(verificationTokenRepository, 'deleteVerificationTokenByUserId').mockResolvedValue();
 
   return {
     sut,

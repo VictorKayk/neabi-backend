11fc42a9af28627ad769331425a5851dac5e9c70
Victor Kayk <victorkayk77@gmail.com>
2022-09-29 00:14:23 -0300

    feat: add read post route

--- src/main/routes/post-routes.ts
+++ src/main/routes/post-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeCreatePostController } from '@/main/factories/post';
+import { makeCreatePostController, makeReadPostBySlugController } from '@/main/factories/post';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
 
 export function post(router: Router) {
   router.post('/post', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreatePostController()));
+  router.get('/post/:slug', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadPostBySlugController()));
 }

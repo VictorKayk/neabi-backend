4793295f55e77b0526e5a3c0220f9da0ca0ccc5d
Victor Kayk <victorkayk77@gmail.com>
2022-09-29 01:57:36 -0300

    feat: add update post by id route

--- src/main/routes/post-routes.ts
+++ src/main/routes/post-routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeCreatePostController, makeReadPostBySlugController, makeReadAllPostsController } from '@/main/factories/post';
+import {
+  makeCreatePostController,
+  makeReadPostBySlugController,
+  makeReadAllPostsController,
+  makeUpdatePostByIdController,
+} from '@/main/factories/post';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
 
@@ -8,4 +13,5 @@ export function post(router: Router) {
   router.post('/post', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreatePostController()));
   router.get('/post/all', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadAllPostsController()));
   router.get('/post/:slug', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadPostBySlugController()));
+  router.patch('/post/:postId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeUpdatePostByIdController()));
 }

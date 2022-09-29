3f26d488d683af316761d8e9429b494f50c96efa
Victor Kayk <victorkayk77@gmail.com>
2022-09-29 00:38:38 -0300

    feat: add read all posts use case

--- src/use-cases/post/interfaces/index.ts
+++ src/use-cases/post/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from '@/use-cases/post/interfaces/post-data-interface';
 export * from '@/use-cases/post/interfaces/post-repository-return-data-interface';
 export * from '@/use-cases/post/interfaces/post-repository-interface';
+export * from '@/use-cases/post/interfaces/post-data-query-interface';
---
+++ src/use-cases/post/interfaces/post-data-query-interface.ts
@@ -0,0 +1,6 @@
+export interface IPostDataQuery {
+  id?: string,
+  slug?: string,
+  title?: string,
+  description?: string,
+}

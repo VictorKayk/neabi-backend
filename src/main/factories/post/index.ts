5f9f1a069cfe81c1e43736efb0b2009a74f4cd79
Victor Kayk <victorkayk77@gmail.com>
2022-09-29 02:26:19 -0300

    feat: add delete post by id factory

--- src/main/factories/post/index.ts
+++ src/main/factories/post/index.ts
@@ -2,3 +2,4 @@ export * from '@/main/factories/post/create-post';
 export * from '@/main/factories/post/read-post-by-slug';
 export * from '@/main/factories/post/read-all-posts';
 export * from '@/main/factories/post/update-post-by-id';
+export * from '@/main/factories/post/delete-post-by-id';

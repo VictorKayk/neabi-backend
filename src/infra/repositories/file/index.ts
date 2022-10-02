11a0c68782d7f7829f9343f8a06bec370b502ec4
Victor Kayk <victorkayk77@gmail.com>
2022-10-02 02:04:01 -0300

    refactor: add index to file repository

---
+++ src/infra/repositories/file/index.ts
@@ -0,0 +1,1 @@
+export * from '@/infra/repositories/file/file-repository';
--- src/infra/repositories/index.ts
+++ src/infra/repositories/index.ts
@@ -2,3 +2,4 @@ export * from '@/infra/repositories/role';
 export * from '@/infra/repositories/user-has-role';
 export * from '@/infra/repositories/user';
 export * from '@/infra/repositories/post';
+export * from '@/infra/repositories/file';

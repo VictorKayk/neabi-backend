28cad087441e76359f921618fbb9933a3b5d8492
Victor Kayk <victorkayk77@gmail.com>
2022-10-02 02:12:01 -0300

    refactor: add upload url to upload file factory

--- src/main/factories/file/upload-file/upload-file-factory.ts
+++ src/main/factories/file/upload-file/upload-file-factory.ts
@@ -6,6 +6,7 @@ import { makeUploadFileValidationFactory } from '@/main/factories/file';
 import { UploadFileController } from '@/adapters/controllers/file/upload-file';
 import { CreateFileTypeUseCase } from '@/use-cases/file/create-file-type';
 import { CreateFileFormatUseCase } from '@/use-cases/file/create-file-format';
+import env from '@/main/config/env';
 
 export function makeUploadFileController(): IController {
   const fileRepository = new FileRepository();
@@ -14,7 +15,12 @@ export function makeUploadFileController(): IController {
   const createTypeUseCase = new CreateFileTypeUseCase(fileRepository, uuidAdapter);
   const createFormatUseCase = new CreateFileFormatUseCase(fileRepository, uuidAdapter);
   const uploadFileController = new UploadFileController(
-    makeUploadFileValidationFactory(), uploadFileUseCase, createTypeUseCase, createFormatUseCase,
+    makeUploadFileValidationFactory(),
+    fileRepository,
+    uploadFileUseCase,
+    createTypeUseCase,
+    createFormatUseCase,
+    env.uploadUrl,
   );
   return uploadFileController;
 }

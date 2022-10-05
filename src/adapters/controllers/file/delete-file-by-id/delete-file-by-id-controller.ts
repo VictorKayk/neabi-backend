f87b259cb1db0d261c5821b6fa25fe1b219f9b29
Victor Kayk <victorkayk77@gmail.com>
2022-10-05 17:33:20 -0300

    feat: add delete file by id controller

---
+++ src/adapters/controllers/file/delete-file-by-id/delete-file-by-id-controller.ts
@@ -0,0 +1,37 @@
+import { IHttpRequest, IHttpResponse } from '@/adapters/interfaces';
+import { IController, IValidation } from '@/adapters/controllers/interfaces';
+import {
+  ok,
+  serverError,
+  badRequest,
+  unauthorized,
+} from '@/adapters/utils/http';
+import { DeleteFileByIdUseCase } from '@/use-cases/file/delete-file-by-id';
+import { DeleteFileService } from '@/use-cases/services/file-service/delete-file';
+
+export class DeleteFileByIdController implements IController {
+  constructor(
+    private readonly validation: IValidation,
+    private readonly deleteFileById: DeleteFileByIdUseCase,
+    private readonly deleteFileService: DeleteFileService,
+  ) { }
+
+  async handle({ params }: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      const validationError = this.validation.validate(params);
+      if (validationError) return badRequest(validationError);
+
+      const { fileId } = params;
+
+      const fileOrError = await this.deleteFileById.execute(fileId);
+      if (fileOrError.isError()) return unauthorized(fileOrError.value);
+
+      const fileServiceOrError = await this.deleteFileService.execute(fileOrError.value.fileName);
+      if (fileServiceOrError.isError()) return badRequest(fileServiceOrError.value);
+
+      return ok(fileOrError);
+    } catch (error) {
+      return serverError(error as Error);
+    }
+  }
+}

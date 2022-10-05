972cffdc6007645cbc9dc7019be5f801827daa8a
Victor Kayk <victorkayk77@gmail.com>
2022-10-05 19:30:47 -0300

    feat: add delete file by id route

--- src/main/routes/file-routes.ts
+++ src/main/routes/file-routes.ts
@@ -3,8 +3,9 @@ import { adaptMulter } from '@/main/adapters/multer';
 import { routerAdapter } from '@/main/adapters/express';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
-import { makeUploadFileController } from '@/main/factories/file';
+import { makeUploadFileController, makeDeleteFileByIdController } from '@/main/factories/file';
 
 export function file(router: Router) {
   router.post('/file/upload', authentication, authorization(['moderator', 'admin']), adaptMulter, routerAdapter(makeUploadFileController()));
+  router.delete('/file/:fileId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeDeleteFileByIdController()));
 }

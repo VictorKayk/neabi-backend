7ff7dc28e9cfe53f703e4b9a39426ddbbc4dfc8b
Victor Kayk <victorkayk77@gmail.com>
2022-10-05 21:12:45 -0300

    docs: add docs to read file by id

--- src/main/docs/paths/file-by-id-path.ts
+++ src/main/docs/paths/file-by-id-path.ts
@@ -1,4 +1,45 @@
 export const fileByIdPath = {
+  get: {
+    security: [{
+      apiKeyAuth: [],
+    }],
+    tags: ['File'],
+    summary: 'API para ler todos os dados de uma arquivo pelo id',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'fileId',
+      description: 'ID do arquivo',
+      required: true,
+      schema: {
+        type: 'string',
+      },
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/file',
+            },
+          },
+        },
+      },
+      400: {
+        $ref: '#/components/badRequest',
+      },
+      401: {
+        $ref: '#/components/unauthorized',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
   delete: {
     security: [{
       apiKeyAuth: [],

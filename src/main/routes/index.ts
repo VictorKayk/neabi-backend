f60caa0b6636db715116d55f642f7fe4df232ab1
Victor Kayk <victorkayk77@gmail.com>
2022-10-08 20:53:42 -0300

    refactor: add attachment to file route

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -2,4 +2,4 @@ export * from '@/main/routes/user-routes';
 export * from '@/main/routes/role-routes';
 export * from '@/main/routes/user-has-role-routes';
 export * from '@/main/routes/post-routes';
-export * from '@/main/routes/file-routes';
+export * from '@/main/routes/attachment/file-routes';

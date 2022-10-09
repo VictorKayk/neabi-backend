bf0083f7e2c4b1a4162a02145ededd54ea0a5208
Victor Kayk <victorkayk77@gmail.com>
2022-10-09 18:21:49 -0300

    feat: add read all urls attachment route

--- src/main/routes/attachment/url-routes.ts
+++ src/main/routes/attachment/url-routes.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
-import { makeCreateUrlController, makeReadUrlByIdController } from '@/main/factories/attachment/url';
+import { makeCreateUrlController, makeReadUrlByIdController, makeReadAllUrlsController } from '@/main/factories/attachment/url';
 
 export function url(router: Router) {
+  router.get('/attachment/url/all', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadAllUrlsController()));
   router.get('/attachment/url/:urlId', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadUrlByIdController()));
   router.post('/attachment/url', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreateUrlController()));
 }

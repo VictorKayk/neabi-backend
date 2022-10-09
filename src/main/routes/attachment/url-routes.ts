118ec3d37248f20d5a417cbdf6ceb1290ebd29f7
Victor Kayk <victorkayk77@gmail.com>
2022-10-09 20:07:31 -0300

    feat: add update url attachment route

--- src/main/routes/attachment/url-routes.ts
+++ src/main/routes/attachment/url-routes.ts
@@ -7,11 +7,13 @@ import {
   makeReadUrlByIdController,
   makeReadAllUrlsController,
   makeDeleteUrlByIdController,
+  makeUpdateUrlByIdController,
 } from '@/main/factories/attachment/url';
 
 export function url(router: Router) {
   router.get('/attachment/url/all', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadAllUrlsController()));
   router.get('/attachment/url/:urlId', authentication, authorization(['user', 'moderator', 'admin']), routerAdapter(makeReadUrlByIdController()));
   router.post('/attachment/url', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreateUrlController()));
+  router.patch('/attachment/url/:urlId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeUpdateUrlByIdController()));
   router.delete('/attachment/url/:urlId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeDeleteUrlByIdController()));
 }

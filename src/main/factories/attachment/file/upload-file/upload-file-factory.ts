7908ba35d9d1774431271f3c83a7b85663ef679e
Victor Kayk <victorkayk77@gmail.com>
2022-10-09 00:39:59 -0300

    refactor: rename upload file to create file use case in factory

--- src/main/factories/attachment/file/upload-file/upload-file-factory.ts
+++ src/main/factories/attachment/file/upload-file/upload-file-factory.ts
@@ -1,4 +1,4 @@
-import { UploadFileUseCase } from '@/use-cases/attachment/file/upload-file';
+import { CreateFileUseCase } from '@/use-cases/attachment/file/create-file';
 import { IController } from '@/adapters/controllers/interfaces';
 import { UuidAdapter } from '@/infra/universally-unique-identifier';
 import { FileRepository } from '@/infra/repositories';
@@ -11,13 +11,13 @@ import env from '@/main/config/env';
 export function makeUploadFileController(): IController {
   const fileRepository = new FileRepository();
   const uuidAdapter = new UuidAdapter();
-  const uploadFileUseCase = new UploadFileUseCase(fileRepository, uuidAdapter);
+  const createFileUseCase = new CreateFileUseCase(fileRepository, uuidAdapter);
   const createTypeUseCase = new CreateFileTypeUseCase(fileRepository, uuidAdapter);
   const createFormatUseCase = new CreateFileFormatUseCase(fileRepository, uuidAdapter);
   const uploadFileController = new UploadFileController(
     makeUploadFileValidationFactory(),
     fileRepository,
-    uploadFileUseCase,
+    createFileUseCase,
     createTypeUseCase,
     createFormatUseCase,
     env.uploadUrl,

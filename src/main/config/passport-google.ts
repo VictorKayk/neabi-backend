482ff526ccd5d7e4a2ce308729a20aa0ad843357
Victor Kayk <victorkayk77@gmail.com>
2022-10-22 18:57:09 -0300

    refactor: get credentials from passport config

--- src/main/config/passport-google.ts
+++ src/main/config/passport-google.ts
@@ -1,17 +1,20 @@
 import passport from 'passport';
-import { StrategyOptions, Strategy } from 'passport-google-oauth20';
+import { StrategyOptionsWithRequest, Strategy } from 'passport-google-oauth20';
 import env from '@/main/config/env';
 
-const options: StrategyOptions = {
+const options: StrategyOptionsWithRequest = {
   clientID: env.googleClientId,
   clientSecret: env.googleClientSecret,
   callbackURL: env.googleCallBackUrl,
-  scope: ['profile', 'email'],
+  scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive'],
+  passReqToCallback: true,
 };
 
-async function verify(accessToken: any, refreshToken: any, profile: any, done: any) {
+async function verify(
+  req: any, accessToken: any, refreshToken: any, params:any, profile: any, done: any,
+) {
   try {
-    done(null, profile);
+    done(null, { ...profile, credentials: params });
   } catch (e) {
     done(new Error('An authorization error occurred, please try again later.'), false);
   }

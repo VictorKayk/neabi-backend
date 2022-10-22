74059bb6eb5483052aa7dcccccb662e4345b852d
Victor Kayk <victorkayk77@gmail.com>
2022-10-22 19:11:25 -0300

    fix: rename makeExternalUserDataController to makeReadExternalUserDataController

--- src/main/factories/user/read-external-user-data/read-external-user-data-factory.ts
+++ src/main/factories/user/read-external-user-data/read-external-user-data-factory.ts
@@ -1,7 +1,7 @@
 import { ReadExternalUserDataController } from '@/adapters/controllers/user/read-external-user-data';
 import { IController } from '@/adapters/controllers/interfaces';
 
-export function makeExternalUserDataController(): IController {
+export function makeReadExternalUserDataController(): IController {
   const readExternalUserDataController = new ReadExternalUserDataController();
   return readExternalUserDataController;
 }

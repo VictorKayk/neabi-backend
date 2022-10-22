76854f7fda340de588fa832a67adf239fa1043a3
Victor Kayk <victorkayk77@gmail.com>
2022-10-22 18:38:16 -0300

    feat: add validation to read all external files factory

--- src/main/factories/attachment/external-file/read-all-external-files/index.ts
+++ src/main/factories/attachment/external-file/read-all-external-files/index.ts
@@ -1,1 +1,2 @@
 export * from '@/main/factories/attachment/external-file/read-all-external-files/read-all-external-files-factory';
+export * from '@/main/factories/attachment/external-file/read-all-external-files/read-all-external-files-validation-factory';
--- src/main/factories/attachment/external-file/read-all-external-files/read-all-external-files-factory.ts
+++ src/main/factories/attachment/external-file/read-all-external-files/read-all-external-files-factory.ts
@@ -2,11 +2,13 @@ import { ReadAllExternalFilesUseCase } from '@/use-cases/attachment/external-fil
 import { IController } from '@/adapters/controllers/interfaces';
 import { ExternalFileRepository } from '@/infra/repositories';
 import { ReadAllExternalFilesController } from '@/adapters/controllers/attachment/external-file/read-all-external-file';
+import { makeReadAllExternalFilesValidationFactory } from '@/main/factories/attachment/external-file/read-all-external-files';
 
 export function makeReadAllExternalFilesController(): IController {
   const externalFileRepository = new ExternalFileRepository();
   const readAllExternalFilesUseCase = new ReadAllExternalFilesUseCase(externalFileRepository);
   const readAllExternalFilesController = new ReadAllExternalFilesController(
+    makeReadAllExternalFilesValidationFactory(),
     readAllExternalFilesUseCase,
   );
   return readAllExternalFilesController;

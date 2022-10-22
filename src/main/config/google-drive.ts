6b53c1f3f11da813b4433cf56a31b36641ebe170
Victor Kayk <victorkayk77@gmail.com>
2022-10-22 18:36:41 -0300

    refactor: set external user credentials to drive config

--- src/main/config/google-drive.ts
+++ src/main/config/google-drive.ts
@@ -1,17 +1,22 @@
 import { google } from 'googleapis';
 import env from '@/main/config/env';
+import { Credentials } from 'google-auth-library';
 
-const oauth2Client = new google.auth.OAuth2(
-  env.googleClientId,
-  env.googleClientSecret,
-  env.googleCallBackUrl,
-);
+const driveConfig = (credentials: Credentials) => {
+  const oauth2Client = new google.auth.OAuth2(
+    env.googleClientId,
+    env.googleClientSecret,
+    env.googleCallBackUrl,
+  );
 
-oauth2Client.setCredentials({ refresh_token: env.googleRefreshToken });
+  oauth2Client.setCredentials(credentials);
 
-const drive = google.drive({
-  version: 'v3',
-  auth: oauth2Client,
-});
+  const drive = google.drive({
+    version: 'v3',
+    auth: oauth2Client,
+  });
 
-export default drive;
+  return { drive };
+};
+
+export default driveConfig;

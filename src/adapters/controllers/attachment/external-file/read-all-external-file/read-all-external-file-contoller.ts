d136a17666047664575c17c0b19340e888db2498
Victor Kayk <victorkayk77@gmail.com>
2022-10-22 18:34:45 -0300

    refactor: set external user credentials to drive controller

--- src/adapters/controllers/attachment/external-file/read-all-external-file/read-all-external-file-contoller.ts
+++ src/adapters/controllers/attachment/external-file/read-all-external-file/read-all-external-file-contoller.ts
@@ -1,16 +1,28 @@
-import { IHttpResponse } from '@/adapters/interfaces';
+/* eslint-disable camelcase */
+import { IHttpResponse, IHttpRequestAuthenticated } from '@/adapters/interfaces';
 import { ReadAllExternalFilesUseCase } from '@/use-cases/attachment/external-file/read-all-external-files';
-import { IController } from '@/adapters/controllers/interfaces';
-import { serverError, ok } from '@/adapters/utils/http';
+import { IController, IValidation } from '@/adapters/controllers/interfaces';
+import { serverError, ok, badRequest } from '@/adapters/utils/http';
+import { IExternalUserCredentials } from '@/use-cases/attachment/external-file/interfaces';
 
 export class ReadAllExternalFilesController implements IController {
   constructor(
+    private readonly validation: IValidation,
     private readonly readAllExternalFiles: ReadAllExternalFilesUseCase,
   ) { }
 
-  async handle(): Promise<IHttpResponse> {
+  async handle({ body }: IHttpRequestAuthenticated): Promise<IHttpResponse> {
     try {
-      const externalFiles = await this.readAllExternalFiles.execute();
+      const validationError = this.validation.validate(body);
+      if (validationError) return badRequest(validationError);
+
+      const {
+        access_token, expires_in, id_token, scope, token_type, refresh_token,
+      }: IExternalUserCredentials = body;
+
+      const externalFiles = await this.readAllExternalFiles.execute({
+        access_token, expires_in, id_token, scope, token_type, refresh_token,
+      });
       return ok(externalFiles);
     } catch (error) {
       return serverError(error as Error);

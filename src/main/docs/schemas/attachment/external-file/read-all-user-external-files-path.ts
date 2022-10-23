7dd90d6072649042cce3fc8c20fb66db4190d142
Victor Kayk <victorkayk77@gmail.com>
2022-10-23 15:48:35 -0300

    refactor: require just access_token in read all user external files

--- src/main/docs/schemas/attachment/external-file/read-all-user-external-files-path.ts
+++ src/main/docs/schemas/attachment/external-file/read-all-user-external-files-path.ts
@@ -5,26 +5,6 @@ export const readAllUserExternalFilesParamsSchema = {
       type: 'string',
       default: 'any_access_token',
     },
-    refresh_token: {
-      type: 'string',
-      default: 'any_refresh_token',
-    },
-    expires_in: {
-      type: 'number',
-      default: 123456789,
-    },
-    scope: {
-      type: 'string',
-      default: 'any_scope',
-    },
-    token_type: {
-      type: 'string',
-      default: 'any_token_type',
-    },
-    id_token: {
-      type: 'string',
-      default: 'any_id_token',
-    },
   },
-  required: ['access_token', 'expires_in', 'scope', 'token_type', 'id_token'],
+  required: ['access_token'],
 };
--- src/use-cases/attachment/external-file/interfaces/external-user-credentials-interface.ts
+++ src/use-cases/attachment/external-file/interfaces/external-user-credentials-interface.ts
@@ -2,8 +2,8 @@
 export interface IExternalUserCredentials {
   access_token: string,
   refresh_token?: string,
-  expires_in: number,
-  scope: string,
-  token_type: string,
-  id_token: string,
+  expires_in?: number,
+  scope?: string,
+  token_type?: string,
+  id_token?: string,
 }

61d7167514872e9472e6fd0f33c140dac77dd516
Victor Kayk <victorkayk77@gmail.com>
2022-10-23 17:10:57 -0300

    refactor: rename viewLink to url and contentLink to downloadUrl

--- src/use-cases/attachment/external-file/interfaces/public-user-external-file-data-interface.ts
+++ src/use-cases/attachment/external-file/interfaces/public-user-external-file-data-interface.ts
@@ -1,8 +1,8 @@
 import { IUserExternalFile } from '@/use-cases/attachment/external-file/interfaces';
 
 export interface IPublicUserExternalFileData extends IUserExternalFile {
-  contentLink?: string,
-  viewLink?: string,
+  downloadUrl?: string,
+  url?: string,
   size?: string,
   originalFileName: string,
 }

f69680b4636d17dd33e3d43f7967c7dbacaafc0d
Victor Kayk <victorkayk77@gmail.com>
2022-10-23 01:16:28 -0300

    refactor: rename makeReadAllExternalFilesController to makeReadAllUserExternalFilesController

--- src/main/routes/attachment/external-file-routes.ts
+++ src/main/routes/attachment/external-file-routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
-import { makeReadAllExternalFilesController } from '@/main/factories/attachment/external-file';
+import { makeReadAllUserExternalFilesController } from '@/main/factories/attachment/external-file';
 
 export function externalFiles(router: Router) {
-  router.post('/attachment/external/user/file/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadAllExternalFilesController()));
+  router.post('/attachment/external/user/file/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadAllUserExternalFilesController()));
 }

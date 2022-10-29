88c62341dee16718df6cdfa0493f82c0f9f10fb5
Victor Kayk <victorkayk77@gmail.com>
2022-10-29 02:11:54 -0300

    feat: add delete external file by id factory

---
+++ src/main/factories/attachment/external-file/delete-external-file-by-id/index.ts
@@ -0,0 +1,2 @@
+export * from '@/main/factories/attachment/external-file/delete-external-file-by-id/delete-external-file-by-id-factory';
+export * from '@/main/factories/attachment/external-file/delete-external-file-by-id/delete-external-file-by-id-validation-factory';

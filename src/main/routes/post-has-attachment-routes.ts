5bcccb62566da40c50d5c383871d83f5074598c7
Victor Kayk <victorkayk77@gmail.com>
2022-10-29 20:31:09 -0300

    feat: add read all attachments from post route

--- src/main/routes/post-has-attachment-routes.ts
+++ src/main/routes/post-has-attachment-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeAddAttachmentToPostController, makeRemoveAttachmentFromPostController } from '@/main/factories/post-has-attachment';
+import { makeAddAttachmentToPostController, makeRemoveAttachmentFromPostController, makeReadAllAttachmentsFromPostController } from '@/main/factories/post-has-attachment';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
 
 export function postHasAttachment(router: Router) {
+  router.get('/post/:postId/attachment/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadAllAttachmentsFromPostController()));
   router.post('/post/:postId/attachment/:attachmentId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeAddAttachmentToPostController()));
   router.delete('/post/:postId/attachment/:attachmentId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeRemoveAttachmentFromPostController()));
 }

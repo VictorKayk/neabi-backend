b618e44a9e32b0452c7136524648c2fd58b5c1cf
Victor Kayk <victorkayk77@gmail.com>
2022-10-29 20:30:10 -0300

    feat: add read all attachments from post factory

---
+++ src/main/factories/post-has-attachment/read-all-attachments-from-post/index.ts
@@ -0,0 +1,2 @@
+export * from '@/main/factories/post-has-attachment/read-all-attachments-from-post/read-all-attachments-from-post-validation-factory';
+export * from '@/main/factories/post-has-attachment/read-all-attachments-from-post/read-all-attachments-from-post-factory';

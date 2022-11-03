cb31ac2fb1b8abde50b865afac553971c13901e4
Victor Kayk <victorkayk77@gmail.com>
2022-11-03 02:07:29 -0300

    feat: add docs to downlaod file by file name route

--- src/main/docs/paths/attachment/file/index.ts
+++ src/main/docs/paths/attachment/file/index.ts
@@ -1,4 +1,5 @@
 export * from '@/main/docs/paths/attachment/file/file-by-id-path';
 export * from '@/main/docs/paths/attachment/file/upload-file-path';
 export * from '@/main/docs/paths/attachment/file/read-all-files-path';
+export * from '@/main/docs/paths/attachment/file/download-file-by-file-name-path';
 export * from '@/main/docs/paths/attachment/file/file-paths-docs';

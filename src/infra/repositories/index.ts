38d08ea30ee1855d0859c4b085155f20a7185bf5
Victor Kayk <victorkayk77@gmail.com>
2022-11-05 17:02:16 -0300

    feat: add tag repository

--- src/infra/repositories/index.ts
+++ src/infra/repositories/index.ts
@@ -2,6 +2,7 @@ export * from '@/infra/repositories/role';
 export * from '@/infra/repositories/user-has-role';
 export * from '@/infra/repositories/user';
 export * from '@/infra/repositories/post';
+export * from '@/infra/repositories/tag';
 export * from '@/infra/repositories/attachment/file';
 export * from '@/infra/repositories/attachment/url';
 export * from '@/infra/repositories/attachment/external-file';

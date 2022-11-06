b9ac838147eb75061767451745369993cb2cdd7f
Victor Kayk <victorkayk77@gmail.com>
2022-11-06 01:51:12 -0300

    feat: add read all tags route

--- src/main/routes/tag-routes.ts
+++ src/main/routes/tag-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
 import {
-  makeCreateTagController,
+  makeCreateTagController, makeReadAllTagsController,
 } from '@/main/factories/tag';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
 
 export function tag(router: Router) {
   router.post('/tag', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreateTagController()));
+  router.get('/tag/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadAllTagsController()));
 }

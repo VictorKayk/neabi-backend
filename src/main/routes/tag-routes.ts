c5547fcf23d673b4f47e8e97b00222019fb1eb57
Victor Kayk <victorkayk77@gmail.com>
2022-11-06 02:21:10 -0300

    feat: add read tag by id route

--- src/main/routes/tag-routes.ts
+++ src/main/routes/tag-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
 import {
-  makeCreateTagController, makeReadAllTagsController,
+  makeCreateTagController, makeReadAllTagsController, makeReadTagByIdController,
 } from '@/main/factories/tag';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
@@ -9,4 +9,5 @@ import { authorization } from '@/main/middlewares/authorization';
 export function tag(router: Router) {
   router.post('/tag', authentication, authorization(['moderator', 'admin']), routerAdapter(makeCreateTagController()));
   router.get('/tag/all', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadAllTagsController()));
+  router.get('/tag/:id', authentication, authorization(['moderator', 'admin']), routerAdapter(makeReadTagByIdController()));
 }

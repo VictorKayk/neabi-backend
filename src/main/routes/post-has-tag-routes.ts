f98eb7ad069c3a1e3ba42eb4cd436ad9bde184b6
Victor Kayk <victorkayk77@gmail.com>
2022-11-09 20:26:29 -0300

    feat: add remove tag from post route

--- src/main/routes/post-has-tag-routes.ts
+++ src/main/routes/post-has-tag-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { routerAdapter } from '@/main/adapters/express';
-import { makeAddTagToPostController } from '@/main/factories/post-has-tag';
+import { makeAddTagToPostController, makeRemoveTagFromPostController } from '@/main/factories/post-has-tag';
 import { authentication } from '@/main/middlewares/authentication';
 import { authorization } from '@/main/middlewares/authorization';
 
 export function postHasTag(router: Router) {
   router.post('/post/:postId/tag/:tagId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeAddTagToPostController()));
+  router.delete('/post/:postId/tag/:tagId', authentication, authorization(['moderator', 'admin']), routerAdapter(makeRemoveTagFromPostController()));
 }

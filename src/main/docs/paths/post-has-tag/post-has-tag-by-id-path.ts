29c78d31f95cf25a715c82070f40772113e39ac1
Victor Kayk <victorkayk77@gmail.com>
2022-11-09 20:30:31 -0300

    feat: add docs to remove tag from post route

--- src/main/docs/paths/post-has-tag/post-has-tag-by-id-path.ts
+++ src/main/docs/paths/post-has-tag/post-has-tag-by-id-path.ts
@@ -51,4 +51,56 @@ export const postHasTagByIdPath = {
       },
     },
   },
+  delete: {
+    security: [{
+      apiKeyAuth: [],
+    }],
+    tags: ['PostHasTag'],
+    summary: 'API para apagar uma tag anexado a uma publicação',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'postId',
+        description: 'ID da publicação',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+      {
+        in: 'path',
+        name: 'tagId',
+        description: 'ID da anexo',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/postHasTag',
+            },
+          },
+        },
+      },
+      400: {
+        $ref: '#/components/badRequest',
+      },
+      401: {
+        $ref: '#/components/unauthorized',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };

10b76d6f1bc40f22ad06a042760990712461acec
Victor Kayk <victorkayk77@gmail.com>
2022-11-10 18:36:42 -0300

    feat: add docs to read all posts from tag route

--- src/main/docs/paths/post-has-tag/index.ts
+++ src/main/docs/paths/post-has-tag/index.ts
@@ -1,3 +1,4 @@
 export * from '@/main/docs/paths/post-has-tag/post-has-tag-by-id-path';
 export * from '@/main/docs/paths/post-has-tag/post-has-tag-paths-docs';
 export * from '@/main/docs/paths/post-has-tag/read-all-tags-from-post-path';
+export * from '@/main/docs/paths/post-has-tag/read-all-posts-from-tag-path';
--- src/main/docs/paths/post-has-tag/post-has-tag-paths-docs.ts
+++ src/main/docs/paths/post-has-tag/post-has-tag-paths-docs.ts
@@ -1,6 +1,7 @@
-import { postHasTagByIdPath, readAllTagsFromPostPath } from '@/main/docs/paths/post-has-tag';
+import { postHasTagByIdPath, readAllTagsFromPostPath, readAllPostsFromTagPath } from '@/main/docs/paths/post-has-tag';
 
 export const postHasTagPathsDocs = {
   '/post/{postId}/tag/all': readAllTagsFromPostPath,
+  '/tag/{tagId}/post/all': readAllPostsFromTagPath,
   '/post/{postId}/tag/{tagId}': postHasTagByIdPath,
 };
